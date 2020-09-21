be0dd61125ce27fe7f79b237ac8d8d7eb9f9ed2d
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-09-21 02:25:06 +0100

    Added Among Us Driver 0.0.1
    First Iteration

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AmongUsDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var bot = new Bot();
+            bot.RunAsync().GetAwaiter().GetResult();
+
+        }
+    }
+}
