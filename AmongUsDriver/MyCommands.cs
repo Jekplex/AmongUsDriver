15b45d412636a70e3d876c0a573fc3f09a69d91e
Jekplex <41015593+Jekplex@users.noreply.github.com>
2020-09-23 16:27:05 +0100

    rebuilt from ground up
    includes mute and unmute and error handling on both.

---
+++ AmongUsDriver/MyCommands.cs
@@ -0,0 +1,84 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmongUsDriver
+{
+    class MyCommands : BaseCommandModule
+    {
+        [Command("ping")]
+        [Description("Used to check if bot is alive.")]
+        public async Task Ping(CommandContext ctx)
+        {
+            await ctx.RespondAsync($"{ctx.User.Mention}, pong!");
+        }
+
+        [Command("mute")]
+        [Description("Mutes everyone in your voice channel. (This server only)")]
+        [RequirePermissions(DSharpPlus.Permissions.MuteMembers)]
+        public async Task Mute(CommandContext ctx)
+        {
+
+            if (ctx.Member.VoiceState == null || ctx.Member.VoiceState.Channel == null)
+            {
+                await ctx.RespondAsync($"{ctx.User.Mention}, you cannot be found in a voice channel on this server.");
+            }
+            else
+            {
+                var playerList = ctx.Member.VoiceState.Channel.Users.ToList();
+
+                try
+                {
+                    for (int i = 0; i < playerList.Count; i++)
+                    {
+                        await playerList.ElementAt(i).SetMuteAsync(true);
+                    }
+                    await ctx.RespondAsync($"{ctx.User.Mention}, all players have been muted.");
+                }
+                catch
+                {
+                    await ctx.RespondAsync($"{ctx.User.Mention}, Error! An individual left too quickly.");
+                }
+                
+            }
+
+        }
+
+        [Command("unmute")]
+        [Description("Unmutes everyone in the your voice channel. (This server only)")]
+        [RequirePermissions(DSharpPlus.Permissions.MuteMembers)]
+        public async Task Unmute(CommandContext ctx)
+        {
+
+            if (ctx.Member.VoiceState == null || ctx.Member.VoiceState.Channel == null)
+            {
+                await ctx.RespondAsync($"{ctx.User.Mention}, you cannot be found in a voice channel on this server.");
+            }
+            else
+            {
+                var playerList = ctx.Member.VoiceState.Channel.Users.ToList();
+
+                try
+                {
+                    for (int i = 0; i < playerList.Count; i++)
+                    {
+                        await playerList.ElementAt(i).SetMuteAsync(false);
+                    }
+                    await ctx.RespondAsync($"{ctx.User.Mention}, all players have been unmuted.");
+                }
+                catch
+                {
+                    await ctx.RespondAsync($"{ctx.User.Mention}, Error! An individual left too quickly.");
+                }
+                
+            }
+
+        }
+
+
+    }
+}
---
+++ AmongUsDriver/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace AmongUsDriver
+{
+    class Program
+    {
+        static DiscordClient discord;
+
+        static CommandsNextExtension commands;
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            // Reads token and prefix from config.json located in project dir.
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            // Discord Config
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+
+                //UseInternalLogHandler = true,
+                //LogLevel = LogLevel.Debug
+                MinimumLogLevel = LogLevel.Debug,
+
+
+            });
+
+            // Setting up commands
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix }
+            });
+
+            commands.RegisterCommands<MyCommands>();
+
+            // Hooking into the event message created
+            //discord.MessageCreated += async e =>
+            //{
+            //    if (e.Message.Content.ToLower().StartsWith("ping"))
+            //        await e.Message.RespondAsync("pong!");
+            //};
+
+            await discord.ConnectAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+    }
+}
