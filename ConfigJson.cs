9b45f0a5e5f0869663ad7bec807b91a2d4558e81
Jekplex <Jekplex@gmail.com>
2020-11-11 08:06:43 +0000

    Updated config.json setup

--- ConfigJson.cs
+++ ConfigJson.cs
@@ -7,11 +7,9 @@ namespace AmongUsDriver
 {
     public struct ConfigJson
     {
-        [JsonProperty("token")]
-        public string Token { get; private set; }
-        [JsonProperty("token2")]
-        public string Token2 { get; private set; }
         [JsonProperty("prefix")]
         public string Prefix { get; private set; }
+        [JsonProperty("token")]
+        public string Token { get; private set; }
     }
 }
